fffa00b32fb19f186ffb99fe9400565693c3c28c
ronakice <ronakice@hotmail.com>
2016-11-27 18:03:21 -0500

    cleancode for fresh start

--- RedditBot.py
+++ RedditBot.py
@@ -6,35 +6,47 @@ r.login()
 inapp_users=set()
 kind_users=set()
 lstk=["thank","please"]
-lstin=["shit","stupid","retarded"]
+lstin=["shit","stupid","retarded","fuck"]
+usr_comment_good={}
+usr_comment_bad={}
 already_seenk=[]
 already_seenin=[]
 while True:
-    comments=r.get_comments('uwaterloo')
+    comments=r.get_comments('askreddit')
     for comment in comments:
         body=comment.body.lower()
         for wordsk in lstk:
             if body.find(wordsk) != -1:
+                usr_comment_good[comment.author]=body
                 kind_users.add(comment.author)
                 break
         for wordsin in lstin:
             if body.find(wordsin) != -1:
+                usr_comment_bad[comment.author]=body
                 inapp_users.add(comment.author)
                 break
-    msg = "Kind users: "
+    if len(kind_users)!=0:
+        msg = "Kind users: "
     for user in kind_users:
         #print (user)
         if user not in already_seenk:
             msg+=str(user)+", "
             already_seenk.append(user)
-    msg +=". "+"Inappropriate users: "
+    msg+="\n"
+    if len(inapp_users)!=0:
+        msg +="Inappropriate users: "
     for user in inapp_users:
         # print (user)
         if user not in already_seenin:
             msg += ", "+ str(user)
             already_seenin.append(user)
-        msg +="."
-    r.user.send_message('rbotq42', msg)
+        msg +=".\n"
+    msg+="\n"
+    for i in usr_comment_bad.keys():
+        msg+="%s : %s"%(i, usr_comment_bad[i])
+        msg+="\n"
+    for i in usr_comment_good.keys():
+        msg+="%s : %s"%(i, usr_comment_good[i])
+        msg+="\n"
+    r.user.send_message('rbot42', msg)
     time.sleep(15)
-
-
